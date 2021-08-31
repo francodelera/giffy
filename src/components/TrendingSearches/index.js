8df4213abfd2ce11941d382736219b8e67bf90b0
Franco De Lera <franco.delera@globant.com>
2021-08-31 15:11:59 -0300

    Added useRef hook

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -1,24 +1,28 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import getTrendingTerms from "services/getTrendingTermsService";
 import Category from "../Category";
 
 export default function LazyTrending() {
     const [show, setShow] = useState(false);
+    const elementRef = useRef();
 
     useEffect(() => {
-        const onChange = (entries) => {
+        const onChange = (entries, observer) => {
             const element = entries[0];
             if (element.isIntersecting) {
                 setShow(true);
+                observer.disconnect();
             }
         }
         const observer = new IntersectionObserver(onChange, {
             rootMargin: '100px'
         });
-        observer.observe(document.getElementById('LazyTrending'));
-    }, [])
+        observer.observe(elementRef.current);
 
-    return <div id='LazyTrending'>
+        return () => observer.disconnect();
+    })
+
+    return <div ref={elementRef}>
         {show ? <TrendingSearches /> : null}
     </div>
 }
