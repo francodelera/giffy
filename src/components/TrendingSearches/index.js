43d5290e439bcdf506c1d2cd0ed309535cd82d40
Franco De Lera <franco.delera@globant.com>
2021-08-31 14:59:21 -0300

    Added Lazy Load to Category component

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -2,7 +2,28 @@ import { useState, useEffect } from "react"
 import getTrendingTerms from "services/getTrendingTermsService";
 import Category from "../Category";
 
-export default function TrendingSearches() {
+export default function LazyTrending() {
+    const [show, setShow] = useState(false);
+
+    useEffect(() => {
+        const onChange = (entries) => {
+            const element = entries[0];
+            if (element.isIntersecting) {
+                setShow(true);
+            }
+        }
+        const observer = new IntersectionObserver(onChange, {
+            rootMargin: '100px'
+        });
+        observer.observe(document.getElementById('LazyTrending'));
+    }, [])
+
+    return <div id='LazyTrending'>
+        {show ? <TrendingSearches /> : null}
+    </div>
+}
+
+function TrendingSearches() {
     const [trends, setTrends] = useState([]);
 
     useEffect(() => {
@@ -12,5 +33,4 @@ export default function TrendingSearches() {
     }, []);
 
     return <Category name='Tendencias' options={trends} />;
-
 }
